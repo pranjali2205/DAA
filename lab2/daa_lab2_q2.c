43fea65ec566949ee7a0b1bd507e3b207cada270
pranjali2205 <81307907+pranjali2205@users.noreply.github.com>
2022-04-17 23:58:00 +0530

    Add files via upload

---
+++ lab2/daa_lab2_q2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j,k,i1,j1,k1;
+    printf("enter size of array:");
+    scanf("%d",&n);
+    int ar[n];
+    printf("enter elements in array:");
+    for(i=0;i<n;i++)
+        scanf("%d",&ar[i]);
+         for(i=0;i<n;i++)
+         {
+              for(j=0;j<n;j++)
+              {
+                   for(k=0;k<n;k++)
+                   {
+                       if(i+ar[j]==ar[k])
+                       {
+                           i1=i;
+                           j1=j;
+                           k1=k;
+                           break;
+                       }
+                   }
+              }
+         }
+
+    printf("the i,j,k indexes are: %d %d %d",i1,j1,k1);
+}
