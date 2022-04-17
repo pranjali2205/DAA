76c8b9c83279f59f09fe0822365b0189cab45ee7
pranjali2205 <81307907+pranjali2205@users.noreply.github.com>
2022-04-17 23:59:26 +0530

    Add files via upload

---
+++ lab2/daa_lab2_q3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j,key,count=0;
+    printf("enter size of array:");
+    scanf("%d",&n);
+    int ar[n];
+    printf("enter elements in array:");
+    for(i=0;i<n;i++)
+        scanf("%d",&ar[i]);
+        printf("enter key:");
+    scanf("%d",&key);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(ar[i]-ar[j]==key || ar[j]-ar[i]==key)
+                count++;
+        }
+    }
+    printf("\ntotal pair whose diff is equal to key are:%d",count);
+}
